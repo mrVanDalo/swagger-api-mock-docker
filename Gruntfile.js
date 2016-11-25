0d03435691a576474bdaa7314000a65a37b9c7f5
Matteo Trotta <matteo.trotta@finconsgroup.local>
2016-11-25 21:11:15 +0100

    Added middleware for CORS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,54 @@
-
 'use strict';
 
 var path = require('path');
 var mockApi = require('swagger-mock-api');
 
-module.exports = function(grunt) {
+function corsMiddleware(req, res, next) {
+    var hasOrigin = req.headers.origin != null
+    res.setHeader('Access-Control-Allow-Origin', hasOrigin ? req.headers.origin : '*');
+    var requestMethods = req.headers['access-control-request-headers'];
+
+    var requestMethods = req.headers['access-control-request-method'];
+    var methods = requestMethods != null ? requestMethods : 'GET, HEAD, PUT';
+    res.setHeader('Access-Control-Allow-Methods', methods);
+
+    res.setHeader('Access-Control-Allow-Credentials', !hasOrigin);
+
+    var requestHeaders = req.headers['access-control-request-headers'];
+    if (requestHeaders != null) {
+        res.setHeader('Access-Control-Allow-Headers', requestHeaders);
+    }
+
+    if (req.method === 'OPTIONS') {
+        res.setHeader('Allow', methods);
+        res.end();
+    }
+    next();
+}
+
+module.exports = function (grunt) {
 
     grunt.initConfig({
         connect: {
             server: {
                 options: {
                     keepalive: true,
-                    middleware: [
-                        mockApi({
+                    middleware: function (connect, options) {
+                        var middlewares = [];
+
+                        middlewares.push(corsMiddleware);
+
+                        middlewares.push(mockApi({
                             swaggerFile: '/data/swagger.yaml',
                             watch: true
-                        })
-                    ],
+                        }));
+
+                        return middlewares;
+                    },
                 },
             },
         },
     });
-
 
     grunt.loadNpmTasks('grunt-contrib-connect');
 
